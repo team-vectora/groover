ccbcef2c7f8e2d84e2f144767eaf8fab0c83f8f0
André Araújo <andre.araujosilva2006@gmail.com>
2025-09-30 23:07:46 -0300

    nao aeguento amis

--- server/app.py
+++ server/app.py
@@ -86,5 +86,4 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", debug=True)
