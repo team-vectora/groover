87b5152832231101a24a7d119c3b0dbdd1d7c8bf
André Araújo <andre.araujosilva2006@gmail.com>
2025-10-01 15:48:48 -0300

    iphone

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 const isDeployed = true;
-export const API_BASE_URL = (isDeployed) ? "https://groover-api.onrender.com/api" : "http://localhost:5000/api";
+export const API_BASE_URL = (isDeployed) ? "https://api.groover.app.br/api" : "http://localhost:5000/api";
